ba9ca56a105877a2718168b1e27d8aadf84b995f
Arbic <teo.maybebaby@gmail.com>
2023-11-08 23:58:30 +0500

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,18 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { ThemeProvider } from '@/components/providers/theme-provider'
-import { ConvexClientProvider } from '@/components/providers/convex-provider'
+import { Toaster } from "sonner";
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { EdgeStoreProvider } from "@/lib/edgestore";
 
-const inter = Inter({ subsets: ['latin'] })
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Tushuncha',
-  description: 'The connected workspace where better, faster work happens',
+  title: "Tushuncha",
+  description: "The connected workspace where better, faster work happens",
   icons: {
     icon: [
       {
@@ -20,31 +24,35 @@ export const metadata: Metadata = {
         media: "(prefers-color-scheme: dark)",
         url: "/logo-dark.svg",
         href: "/logo-dark.svg",
-      }
-    ]
-  }
-}
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ConvexClientProvider>
-        <ThemeProvider
-          attribute='class'
-          defaultTheme='system'
-          enableSystem
-          disableTransitionOnChange
-          storageKey='tushuncha-theme'
-        >
-          {children}
-        </ThemeProvider>
+          <EdgeStoreProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="tushuncha-theme"
+          >
+            <Toaster position="bottom-center" />
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
+          </EdgeStoreProvider>
         </ConvexClientProvider>
-        </body>
+      </body>
     </html>
-  )
+  );
 }
